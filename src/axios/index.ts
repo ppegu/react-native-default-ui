54cb75255f03c88652833840d41f4a4e5dd664b8
ppegu <ppegu@cisco.com>
2023-12-28 13:37:09 +0530

    fix: export the needed only

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,3 +1,3 @@
-export * from "./axios.util";
+export { Api, protectedGetApi, protectedPostApi, API_URL } from "./axios.util";
 export { default as useApi } from "./useApi";
 export { default as useProtectedApi } from "./useProtectedApi";
