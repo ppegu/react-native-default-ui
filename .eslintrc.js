21a94678df525d80d69c84884009edf6135e6a65
ppegu <ppegu@cisco.com>
2023-12-29 13:19:08 +0530

    fix: indexing fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,36 @@
 module.exports = {
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   overrides: [],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: "latest",
     ecmaFeatures: {
       jsx: true,
     },
-    sourceType: 'module',
+    sourceType: "module",
     warnOnUnsupportedTypeScriptVersion: false,
   },
-  plugins: ['react'],
+  plugins: ["react"],
   rules: {
-    'no-unused-vars': 'warn',
-    'react/prop-types': 'off',
-    'no-empty-pattern': 'warn',
-    'react/no-unescaped-entities': 'off',
-    'no-undef': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [
+    "no-unused-vars": "warn",
+    "react/prop-types": "off",
+    "no-empty-pattern": "warn",
+    "react/no-unescaped-entities": "off",
+    "no-undef": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [
       1,
-      { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
+      { extensions: [".ts", ".tsx", ".js", ".jsx"] },
     ],
   },
 };
