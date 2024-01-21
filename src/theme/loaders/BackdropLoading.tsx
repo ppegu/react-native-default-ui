e8e4db9bfeb4b38e315060dbb3bfcd45dc7814c9
ppegu <ppegu@cisco.com>
2024-01-21 10:41:55 +0530

    fix: backdrop loader props

--- src/theme/loaders/BackdropLoading.tsx
+++ src/theme/loaders/BackdropLoading.tsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { Center } from "../views/View";
-import { ABSOLUTE_STYLE } from "../responsive";
+import type { ActivityIndicatorProps } from "react-native";
 import { ActivityIndicator } from "react-native";
+import { ABSOLUTE_STYLE } from "../responsive";
+import { Center } from "../views/View";
 
-export default function BackdropLoading({ loading }: { loading: boolean }) {
+export interface BackdropLoadingProps extends ActivityIndicatorProps {
+  loading: boolean;
+}
+
+export default function BackdropLoading({
+  loading,
+  ...rest
+}: BackdropLoadingProps) {
   if (!loading) return <></>;
 
   return (
     <Center {...ABSOLUTE_STYLE}>
-      <ActivityIndicator />
+      <ActivityIndicator {...rest} />
     </Center>
   );
 }
